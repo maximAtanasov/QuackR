66acdf57b8823be952ce6fb9e291d6b2559520a9
Dimitar <parushev1993@gmail.com>
2019-04-14 17:42:20 +0200

    updated properties

--- src/main/java/de/webtech/quackr/service/event/EventNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package de.webtech.quackr.service.event;
-
-public class EventNotFoundException extends Throwable {
-    public EventNotFoundException(long eventId) {
-        super("Event with id " + eventId + " not found!");
-    }
-}
