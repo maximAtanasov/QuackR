d868c277d6585513cedb882f2e836770ac4938e0
Maxim Atanasov <atanasovmaksim1@gmail.com>
2019-04-30 22:11:32 +0200

    fix build

---
+++ quackr-server/src/main/java/de/webtech/quackr/service/comment/CommentService.java
@@ -0,0 +1,121 @@
+package de.webtech.quackr.service.comment;
+
+import de.webtech.quackr.persistance.comment.CommentEntity;
+import de.webtech.quackr.persistance.comment.CommentRepository;
+import de.webtech.quackr.persistance.event.EventEntity;
+import de.webtech.quackr.persistance.event.EventRepository;
+import de.webtech.quackr.persistance.user.UserEntity;
+import de.webtech.quackr.service.comment.domain.CreateCommentResource;
+import de.webtech.quackr.service.comment.domain.GetCommentResource;
+import de.webtech.quackr.service.event.EventNotFoundException;
+import de.webtech.quackr.service.user.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    private final EventRepository eventRepository;
+
+    private final CommentMapper commentMapper = new CommentMapper();
+
+    @Autowired
+    public CommentService(CommentRepository commentRepository, EventRepository eventRepository) {
+        this.commentRepository = commentRepository;
+        this.eventRepository = eventRepository;
+    }
+
+
+    /**
+     * Return all comments for the selected event.
+     * @param eventId The id of the event.
+     * @return All comments for the selected event.
+     * @throws EventNotFoundException Thrown when the selected event is not found.
+     */
+    public Collection<GetCommentResource> getComments(long eventId) throws EventNotFoundException {
+        if(eventRepository.existsById(eventId)){
+            return commentMapper.map(commentRepository.findByEventId(eventId));
+        } else {
+            throw new EventNotFoundException(eventId);
+        }
+    }
+
+    /**
+     * Creates a comment.
+     * @param resource A CreateCommentResource object.
+     * @param eventId The eventId of the event we're commenting.
+     * @return A GetCommentResource object.
+     * @throws EventNotFoundException Thrown if the event with eventId is not found.
+     */
+    public GetCommentResource createComment(CreateCommentResource resource, long eventId) throws EventNotFoundException {
+        Optional<EventEntity> event = eventRepository.findById(eventId);
+        if(event.isPresent()){
+            CommentEntity newComment = new CommentEntity();
+            newComment.setEventId(eventId);
+            newComment.setDatePosted(new Date());
+            newComment.setPosterId(resource.getPosterId());
+            newComment.setText(resource.getText());
+            event.get().getComments().add(newComment);
+            commentRepository.save(newComment);
+            eventRepository.save(event.get());
+            return commentMapper.map(newComment);
+        } else {
+            throw new EventNotFoundException(eventId);
+        }
+    }
+
+
+    /**
+     * Retrieves a single comment from the database.
+     * @param commentId The id of the comment.
+     * @return A GetCommentResource object.
+     * @throws CommentNotFoundException Thrown if the comment is not found.
+     */
+    public GetCommentResource getComment(long commentId) throws CommentNotFoundException {
+        Optional<CommentEntity> commentEntity = commentRepository.findById(commentId);
+        if(commentEntity.isPresent()){
+            return commentMapper.map(commentEntity.get());
+        }else{
+            throw new CommentNotFoundException(commentId);
+        }
+    }
+
+    /**
+     * Delete a comment from the database.
+     * @param commentId The id of the comment to delete.
+     * @throws CommentNotFoundException Thrown if the comment is not found.
+     */
+    public void deleteComment(long commentId) throws CommentNotFoundException {
+        if(commentRepository.existsById(commentId)){
+            commentRepository.deleteById(commentId);
+        }else{
+            throw new CommentNotFoundException(commentId);
+        }
+    }
+
+    /**
+     * Edits an existing comment in the database.
+     * @param resource A CreateCommentResource containing the new data.
+     * @param commentId The id of the comment to change.
+     * @return A GetCommentResource object.
+     * @throws CommentNotFoundException Thrown if the event is not found.
+     */
+    public GetCommentResource editComment(CreateCommentResource resource, long commentId) throws CommentNotFoundException {
+        Optional<CommentEntity> commentEntity = commentRepository.findById(commentId);
+        if(commentEntity.isPresent()){
+            commentEntity.get().setText(resource.getText());
+            commentRepository.save(commentEntity.get());
+            return commentMapper.map(commentEntity.get());
+        }else{
+            throw new CommentNotFoundException(commentId);
+        }
+    }
+}
