696fb652c8a84efc7021265572e9ec141651d59e
maximAtanasov <maxim.atanasov@protonmail.com>
2019-06-06 22:26:47 +0200

    implemented dummy login, register and home pages

--- quackr-app/src/app/app.module.ts
+++ quackr-app/src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './view/login-page/login-page.component';
 import {HomepageComponent} from "./view/homepage/homepage.component";
+import { HeaderComponent } from './view/header/header.component';
+import { RegisterPageComponent } from './view/register-page/register-page.component';
+import {RouterModule, Routes} from "@angular/router";
+import { FooterComponent } from './view/footer/footer.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'home', component: HomepageComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    HomepageComponent
+    HomepageComponent,
+    HeaderComponent,
+    RegisterPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
